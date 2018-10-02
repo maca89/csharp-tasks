8f7307c7906790d1020665dd55c2f38a43ecd337
maca89 <kmaca89@gmail.com>
2018-10-02 23:32:47 +0200

    added task #2 for printing hard-coded user name

---
+++ task02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace task02
+{
+    class Program
+    {
+        // Prints name of the user.
+        static void Main(string[] args)
+        {
+            var name = "<PUT YOUR NAME HERE>";
+            Console.WriteLine("My name is: " + name);
+        }
+    }
+}
