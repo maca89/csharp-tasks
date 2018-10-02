1e91853e8dc982885a18d76ab537cf239f3def2e
maca89 <kmaca89@gmail.com>
2018-10-02 23:46:06 +0200

    added task #4 with user error detection

---
+++ task04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task04
+{
+    class Program
+    {
+        // Ask for a user's name and then prints it.
+        // Enhanced with user error detection.
+        static void Main(string[] args)
+        {
+            // Tell users what is expected of them.
+            Console.Write("Please, write your name and press ENTER: ");
+
+            // Wait until they write their names.
+            var name = Console.ReadLine();
+
+            if (name == "") {
+                // No name provided, print error.
+                Console.WriteLine("You didn't write any name.");
+            }
+            else {
+                // Print their names back.
+                Console.WriteLine("Nice to meet you, " + name + ".");
+            }
+        }
+    }
+}
