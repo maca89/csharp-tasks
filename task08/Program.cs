71503dcc527bddce7ac927d7dfdf7fe2a4b415e6
maca89 <kmaca89@gmail.com>
2018-10-03 00:20:55 +0200

    split #7 into two

--- task08/Program.cs
+++ task08/Program.cs
@@ -24,15 +24,20 @@ namespace task05
             }
         }
 
-        // Ask for user's name and then prints it.
+        // Ask for user's name and age and then prints it.
         // Uses a simple function to ensure a name was given.
+        // Asks for the age in straight-forward fashion.
         static void Main(string[] args)
         {
             // Ask user for the name.
             string name = AskForName();
 
-            // Print their names back.
-            Console.WriteLine("Nice to meet you, " + name + ".");
+            // Ask user for the age.
+            Console.Write("Please, write your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            // Print their names and ages back.
+            Console.WriteLine("Nice to meet you, " + name + ". You are " + age + " years old.");
         }
     }
 }
