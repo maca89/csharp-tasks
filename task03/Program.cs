35f6ed5af27290618083e060d765011e198b3fad
maca89 <kmaca89@gmail.com>
2018-10-02 23:41:36 +0200

    added task #3 that asks user for the name and then prints it

---
+++ task03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task03
+{
+    class Program
+    {
+        // Ask for a user's name and then prints it.
+        static void Main(string[] args)
+        {
+            // Tell users what is expected of them.
+            Console.Write("Please, write your name and press ENTER: ");
+
+            // Wait until they write their names.
+            var name = Console.ReadLine();
+
+            // Print their names back.
+            Console.WriteLine("Nice to meet you, " + name + ".");
+        }
+    }
+}
