d8d3b3dfac6bb3cbfb8d5f48fef8bfe91abd8834
maca89 <kmaca89@gmail.com>
2018-10-02 23:26:01 +0200

    added simple task01 with hello world

--- readme.txt
+++ readme.txt
@@ -1,1 +1,9 @@
+-- Intro --
 This repository hold simple tutorial C# tasks.
+
+-- How to create new task --
+> dotnet new console -o task01
+
+-- How to run a task --
+> cd task01
+> dotnet run
---
+++ task01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace task01
+{
+    class Program
+    {
+
+        // Simple Hello World print application.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
