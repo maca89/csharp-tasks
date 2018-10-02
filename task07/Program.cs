a5d02cc3ee17b17af3aadbf61b0b37682c403a18
maca89 <kmaca89@gmail.com>
2018-10-03 00:08:41 +0200

    added task #7 that asks also for age

---
+++ task07/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace task05
+{
+    class Program
+    {
+        // Ask for user's name and returns it.
+        static string AskForName()
+        {
+            // Tell users what is expected of them.
+            Console.Write("Please, write your name: ");
+
+            // Wait until they write their names.
+            return Console.ReadLine();
+        }
+
+        // Ask for user's name and then prints it.
+        // Uses a simple function to ensure a name was given.
+        static void Main(string[] args)
+        {
+            // Ask user for the name.
+            string name = "";
+
+            while (name == "") {
+                name = AskForName();
+
+                if (name == "") {
+                    Console.WriteLine("You didn't write any name.");
+                }
+            }
+
+            // Ask user for the age.
+            Console.Write("Please, write your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            // Print their names back.
+            Console.WriteLine("Nice to meet you, " + name + ". You are " + age + " years old.");
+        }
+    }
+}
